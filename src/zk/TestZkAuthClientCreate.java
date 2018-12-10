e8b2b8a17dcc1ccb9c8e60d4e26fd6695721e311
Anur <anurnomeru@163.com>
2018-12-10 10:34:33 +0800

    -

---
+++ src/zk/TestZkAuthClientCreate.java
@@ -0,0 +1,48 @@
+package zk;
+
+import java.io.IOException;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+import zk.common.Constant;
+import zk.common.CustomWatcher;
+/**
+ * Created by Anur IjuoKaruKas on 2018/12/10
+ */
+public class TestZkAuthClientCreate {
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        createSync();
+        //        createAsync();
+    }
+
+    public static void createSync() throws IOException, InterruptedException, KeeperException {
+        CustomWatcher customWatcher = new CustomWatcher("create - test - sync");
+        ZooKeeper zooKeeper = new ZooKeeper(Constant.CONNECT_STR, 5000, customWatcher);
+        customWatcher.await();
+
+        String path1 = zooKeeper.create("/sanguo", "luoguanzhong".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("Create path " + path1);
+
+        String path2 = zooKeeper.create("/sanguoz", "luoguanzhong".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        System.out.println("Create path " + path2);
+    }
+
+    public static void createAsync() throws IOException, InterruptedException, KeeperException {
+        CustomWatcher customWatcher = new CustomWatcher("create - test - async");
+
+        ZooKeeper zooKeeper = new ZooKeeper(Constant.CONNECT_STR, 5000, customWatcher);
+        customWatcher.await();
+
+        //        zooKeeper.addAuthInfo("digest", "foo:true".getBytes());
+
+        zooKeeper.create("/sanguo111", "luoguanzhong".getBytes(), Ids.CREATOR_ALL_ACL, CreateMode.EPHEMERAL_SEQUENTIAL,
+            (i, s, o, s1) -> System.out.println(String.format("create path async rc = %s, path = %s, ctx = %s, real path = %s", i, s, o, s1)), "This is context");
+
+        zooKeeper.create("/sanguo111", "luoguanzhong".getBytes(), Ids.CREATOR_ALL_ACL, CreateMode.EPHEMERAL,
+            (i, s, o, s1) -> System.out.println(String.format("create path async rc = %s, path = %s, ctx = %s, real path = %s", i, s, o, s1)), "This is context");
+
+        Thread.sleep(100000000);
+    }
+}
