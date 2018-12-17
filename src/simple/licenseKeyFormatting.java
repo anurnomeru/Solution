221fb600c96c8b9d2ad7c6f56a88a568143dfdcf
Anur <anurnomeru@163.com>
2018-12-17 18:16:17 +0800

    修复了一个因为selectionKey使用for循环而不是迭代器引起的bug

--- src/simple/licenseKeyFormatting.java
+++ src/simple/licenseKeyFormatting.java
@@ -1,5 +1,10 @@
 package simple;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 /**
  * Created by Anur IjuoKaruKas on 2018/12/14
  * 给定一个密钥字符串S，只包含字母，数字以及 '-'（破折号）。N 个 '-' 将字符串分成了 N+1 组。给定一个数字 K，重新格式化字符串，除了第一个分组以外，每个分组要包含 K 个字符，第一个分组至少要包含 1 个字符。两个分组之间用 '-'（破折号）隔开，并且将所有的小写字母转换为大写字母。
@@ -31,4 +36,83 @@ package simple;
  */
 public class licenseKeyFormatting {
 
+    public static char c = "-".toCharArray()[0];
+
+    public static void main(String[] args) {
+        System.out.println(solution("111-222-333", 2));
+    }
+
+    public static String solution(String s, int k) {
+        if (k < 0) {
+            throw new RuntimeException();
+        }
+
+        char[] chars = s.toCharArray();
+        List<Integer> needToRemoveIndex = new ArrayList<>();
+        int effectChar = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == c) {
+                needToRemoveIndex.add(i);
+            } else {
+                effectChar++;
+            }
+        }
+
+        char[] chars1 = remove(chars, needToRemoveIndex);
+
+        if (k == 0) {
+            return new String(chars1);
+        }
+
+        int needToAdd = (effectChar - 1) / k;
+
+        List<Integer> needToAddIndex = new ArrayList<>();
+        for (int i = 1; i <= needToAdd; i++) {
+            needToAddIndex.add(i * k - 1);
+        }
+
+        if (needToAddIndex.size() == 0) {
+            return new String(chars1);
+        }
+
+        return new String(add(chars1, needToAddIndex));
+    }
+
+    public static char[] add(char[] chars, List<Integer> indexs) {
+        indexs = indexs.stream()
+                       .sorted()
+                       .collect(Collectors.toList());
+
+        int length = chars.length;
+        char[] newChars = new char[length + indexs.size()];
+        int newCharsLength = newChars.length;
+
+        int addCount = 0;
+
+        System.arraycopy(chars, 0, newChars, 0, length);
+        for (Integer index : indexs) {
+            int trueIndex = index + addCount;
+            System.arraycopy(newChars, trueIndex, newChars, trueIndex+1, newCharsLength - trueIndex - 1);
+            newChars[trueIndex + 1] = c;
+            addCount++;
+        }
+        return newChars;
+    }
+
+    public static char[] remove(char[] chars, List<Integer> indexs) {
+        indexs = indexs.stream()
+                       .sorted(Comparator.reverseOrder())
+                       .collect(Collectors.toList());
+
+        int length = chars.length;
+        for (Integer index : indexs) {
+            System.arraycopy(chars, index + 1, chars, index, length - index - 1);
+        }
+
+        int newLength = chars.length - indexs.size();
+        char[] newChars = new char[newLength];
+
+        System.arraycopy(chars, 0, newChars, 0, newLength);
+        return newChars;
+    }
 }
