eedf8fa58e5e2dd801a4828877620cdb32fb4727
Anur <anurnomeru@163.com>
2018-12-19 21:45:48 +0800

    动态代理demo

--- src/proxy/DynamicProxy.java
+++ src/proxy/DynamicProxy.java
@@ -1,8 +1,31 @@
 package proxy;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 /**
  * Created by Anur IjuoKaruKas on 2018/12/19
  */
-public class DynamicProxy {
+public class DynamicProxy implements InvocationHandler {
 
+    public static void main(String[] args) {
+        Animal wolf = (Animal) new DynamicProxy().bind(new Wolf());
+        System.out.println(wolf.say());
+    }
+
+    public Object object;
+
+    public Object bind(Object o) {
+        this.object = o;
+        return Proxy.newProxyInstance(o.getClass()
+                                       .getClassLoader(), o.getClass()
+                                                           .getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object o = method.invoke(object, args);
+        o = o + "-Dynamic";
+        return o;
+    }
 }
