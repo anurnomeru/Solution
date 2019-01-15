302a36db06a9e959cf7a37c2c12df3d9e99e0abd
Anur <anurnomeru@163.com>
2019-01-15 11:16:54 +0800

    netty复习

---
+++ src/netty/server/EchoServer.java
@@ -0,0 +1,56 @@
+package netty.server;
+
+import java.net.InetSocketAddress;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+/**
+ * Created by Anur IjuoKaruKas on 2019/1/15
+ */
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    public void start() throws InterruptedException {
+        EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(group)
+                           .channel(NioServerSocketChannel.class)
+                           .localAddress(new InetSocketAddress(port))
+                           .childHandler(new ChannelInitializer<SocketChannel>() {// 这是关键，当一个新的连接被接受（accept）时，一个新的子channel将会被创建，而ChannelInitializer
+                               // 将会把一个你的EchoServerHandler 的实例添加到该Channel的ChannelPipeline中。正如我们之前所解释的，这个ChannelHandler将会受到有关入栈消息的通知
+
+                               @Override
+                               protected void initChannel(SocketChannel socketChannel) {
+                                   socketChannel.pipeline()
+                                                .addLast(serverHandler);
+                               }
+                           });
+
+            ChannelFuture f = serverBootstrap.bind()
+                                             .sync();
+            f.channel()
+             .closeFuture()
+             .sync();
+        } finally {
+            group.shutdownGracefully()
+                 .sync();
+        }
+    }
+}
--- src/netty/server/EchoServerHandler.java
+++ src/netty/server/EchoServerHandler.java
@@ -1,4 +1,4 @@
-package netty;
+package netty.server;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
