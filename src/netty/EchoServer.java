aa2e646d5151c3ae5627987b8eeb4c5881cf504c
Anur <anurnomeru@163.com>
2019-01-15 10:53:34 +0800

    netty复习

---
+++ src/netty/EchoServer.java
@@ -0,0 +1,20 @@
+package netty;
+
+/**
+ * Created by Anur IjuoKaruKas on 2019/1/15
+ */
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void start() {
+    }
+}
---
+++ src/netty/EchoServerHandler.java
@@ -0,0 +1,32 @@
+package netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+/**
+ * Created by Anur IjuoKaruKas on 2019/1/15
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)// 读完后好像是返回一个空的buffer，然后关闭channel？？？
+           .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();// 关闭这个Channel
+    }
+}
