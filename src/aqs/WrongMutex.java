981723daf8e21cfe9523eb23f3d2edd71038128a
Anur <anurnomeru@163.com>
2019-05-27 14:40:02 +0800

    -

--- src/aqs/WrongMutex.java
+++ src/aqs/WrongMutex.java
@@ -2,12 +2,11 @@ package aqs;
 
 import java.util.Date;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
-import sun.misc.Unsafe;
 
 /**
- * Created by Anur IjuoKaruKas on 2019/5/27
+ * Created by Anur IjuoKaruKas on 2019/5/7
  */
-public class Mutex extends AbstractQueuedSynchronizer {
+public class WrongMutex extends AbstractQueuedSynchronizer {
 
     public static class Sync extends AbstractQueuedSynchronizer {
 
@@ -15,16 +14,22 @@ public class Mutex extends AbstractQueuedSynchronizer {
             setState(100); // set the initial state, being unlocked.
         }
 
+        private boolean sign;
+
         @Override
         protected boolean tryAcquire(int ignore) {
-            boolean result = compareAndSetState(100, 1);
+            boolean result = false;
+            if (!sign) {
+                sign = true;
+                result =  true;
+            }
             print("尝试获取锁" + (result ? "成功" : "失败"));
             return result;
         }
 
         @Override
         protected boolean tryRelease(int ignore) {
-            setState(100);
+            sign = false;
             return true;
         }
     }
@@ -40,7 +45,7 @@ public class Mutex extends AbstractQueuedSynchronizer {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Mutex mutex = new Mutex();
+        WrongMutex mutex = new WrongMutex();
         mutex.lock();
 
         Thread thread = new Thread(() -> {
