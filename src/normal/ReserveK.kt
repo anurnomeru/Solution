53ee8c7435d13d3c248aa4e7f34f1d03e96e5fd6
anur <anurnomeru@163.com>
2019-08-16 11:33:13 +0800

    数据结构 - 倒转链表

---
+++ src/normal/ReserveK.kt
@@ -0,0 +1,46 @@
+package normal
+
+object ReserveK {
+
+    var head: Node? = null
+
+    fun reserve() {
+        if (head == null) return
+        doReserve(head!!)
+    }
+
+    private fun doReserve(current: Node) {
+        val next = current.next
+        if (next == null) {
+            head = current
+            return
+        }
+        doReserve(next)
+        next.next = current
+        current.next = null
+    }
+
+    fun insert(value: Int) {
+        if (head == null) {
+            head = Node(value)
+        } else doInsert(head!!, value)
+    }
+
+    private fun doInsert(prev: Node, value: Int) {
+        prev.next?.let { doInsert(it, value) } ?: let { prev.next = Node(value) }
+    }
+
+    class Node(val value: Int) {
+        var next: Node? = null
+    }
+}
+
+fun main() {
+    ReserveK.insert(1)
+    ReserveK.insert(2)
+    ReserveK.insert(3)
+    ReserveK.insert(4)
+    ReserveK.insert(5)
+    ReserveK.reserve()
+    println()
+}
