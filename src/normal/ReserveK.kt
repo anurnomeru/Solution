83181aa305bbf5181379a07f1b4ad8262a891b7a
anur <anurnomeru@163.com>
2019-08-16 16:08:03 +0800

    数据结构 - 倒转链表

--- src/normal/ReserveK.kt
+++ src/normal/ReserveK.kt
@@ -4,6 +4,14 @@ object ReserveK {
 
     var head: Node? = null
 
+    fun print() {
+        var gonaPrint = head
+        while (gonaPrint != null) {
+            println(gonaPrint.value)
+            gonaPrint = gonaPrint.next
+        }
+    }
+
     fun reserve() {
         if (head == null) return
         doReserve(head!!)
@@ -19,6 +27,17 @@ object ReserveK {
         next.next = current
         current.next = null
     }
+
+//    private fun doReserveK(current: Node, k: Int, count: Int) {
+//        val next = current.next
+//        if (next == null) {
+//            head = current
+//            return
+//        }
+//        doReserveK(next, k)
+//        next.next = current
+//        current.next = null
+//    }
 
     fun insert(value: Int) {
         if (head == null) {
@@ -41,6 +60,13 @@ fun main() {
     ReserveK.insert(3)
     ReserveK.insert(4)
     ReserveK.insert(5)
+    ReserveK.insert(6)
+    ReserveK.insert(7)
+    ReserveK.insert(8)
+    ReserveK.insert(9)
+    ReserveK.insert(10)
+    ReserveK.insert(11)
+    ReserveK.insert(12)
     ReserveK.reserve()
-    println()
+    ReserveK.print()
 }
