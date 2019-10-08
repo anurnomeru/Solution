d889c676a0712fcbc9fc2e3d41e22740231e09ef
anur <anurnomeru@163.com>
2019-10-08 17:06:04 +0800

    kmp算法～～

---
+++ src/structure/Kmp.kt
@@ -0,0 +1,50 @@
+package structure
+
+
+/**
+ * Created by Anur IjuoKaruKas on 2019/10/8
+ */
+object Kmp {
+
+    fun solution(tar: String, src: String) {
+        val tarArr = tar.toCharArray()
+        val commonIndex = IntArray(tarArr.size)
+
+        var matchedIndex = 0
+        for (i in tarArr.indices) {
+            if (i != matchedIndex && tarArr[i] == tar[matchedIndex]) {
+                commonIndex[i] = matchedIndex
+                matchedIndex++
+            } else {
+                commonIndex[i] = 0
+                matchedIndex = 0
+
+                if (tar[matchedIndex] == tarArr[0]) matchedIndex++
+            }
+        }
+
+        val srcArr = src.toCharArray()
+
+        var tarIndex = 0
+        var matchingIndex = 0
+
+        while (matchingIndex < srcArr.size) {
+            if (tarArr[tarIndex] == srcArr[matchingIndex]) {
+                if (tarIndex == tarArr.size - 1) {
+                    println("from ${matchingIndex - tarIndex} to ${matchingIndex}")
+                    tarIndex = commonIndex[tarIndex]
+                } else {
+                    tarIndex++
+                }
+            } else {
+                tarIndex = commonIndex[tarIndex]
+            }
+            matchingIndex++
+        }
+        println()
+    }
+}
+
+fun main() {
+    Kmp.solution("123", "12223123")
+}
