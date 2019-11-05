65bfe0da4e212fd113273dd328d7e94e3021993e
anur <anurnomeru@163.com>
2019-11-05 09:43:19 +0800

    931. 下降路径最小和

--- src/leetcode/normal/MinFallingPathSum.java
+++ src/leetcode/normal/MinFallingPathSum.java
@@ -19,8 +19,6 @@ package leetcode.normal;
  * [3,5,7], [3,5,8], [3,5,9], [3,6,8], [3,6,9]
  * 和最小的下降路径是 [1,4,7]，所以答案是 12。
  *
- *  
- *
  * 提示：
  *
  * 1 <= A.length == A[0].length <= 100
@@ -29,6 +27,9 @@ package leetcode.normal;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/minimum-falling-path-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 执行用时 : 5 ms, 在所有 java 提交中击败了 84.00% 的用户
+ * 内存消耗 : 37.5 MB, 在所有 java 提交中击败了 95.15% 的用户
  */
 public class MinFallingPathSum {
 
@@ -36,12 +37,36 @@ public class MinFallingPathSum {
      * 直接暴力 = =
      */
     public int minFallingPathSum(int[][] A) {
-        if (A == null) {
-            return 0;
-        } else if (A.length == 0) {
-            return 0;
-        }
+        int row = A.length;
+        int columnSize = A[0].length;
         int columnMaxIndex = A[0].length - 1;
 
+        if (row != 1) {// 最特殊的情况{
+            for (int i = row - 2; i >= 0; i--) {
+                for (int j = 0; j < columnSize; j++) {
+                    int numPrev = 100;
+                    int numNext = 100;
+                    if (j != 0) {
+                        numPrev = A[i + 1][j - 1];
+                    }
+                    if (j != columnMaxIndex) {
+                        numNext = A[i + 1][j + 1];
+                    }
+
+                    int num = A[i + 1][j];
+
+                    int min = Math.min(Math.min(numPrev, num), numNext);
+                    A[i][j] = min + A[i][j];
+                }
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < columnSize; i++) {
+            if (A[0][i] < min) {
+                min = A[0][i];
+            }
+        }
+        return min;
     }
 }
